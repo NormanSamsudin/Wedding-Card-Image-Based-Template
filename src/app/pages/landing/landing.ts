f2eab107f7c09ec076a26e9102844659e5d34955
norman.samsudin <137969549+NormanSamsudin@users.noreply.github.com>
2025-10-12 14:59:36 +0800

    feat: update wedding information

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -289,7 +289,7 @@ export class LandingComponent implements OnInit, AfterViewInit, OnDestroy {
 
   // Countdown Timer
   private startCountdown() {
-    const weddingDate = new Date('2025-12-28T16:00:00');
+    const weddingDate = new Date('2025-12-21T16:00:00');
 
     this.countdownSubscription = interval(1000).subscribe(() => {
       const now = new Date();
