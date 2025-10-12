4cf1972b5bb2d40dadb93aacc264fe8237f0ea7b
norman.samsudin <137969549+NormanSamsudin@users.noreply.github.com>
2025-10-12 15:14:25 +0800

    feat: update font on the center screen

--- src/app/services/rsvp-firebase.service.ts
+++ src/app/services/rsvp-firebase.service.ts
@@ -15,7 +15,7 @@ export interface RSVPData {
   providedIn: 'root'
 })
 export class RSVPFirebaseService {
-  private readonly API_URL = 'https://normansamsudin.duckdns.org/webhook/post-muazzshafiqah';
+  private readonly API_URL = 'https://normansamsudin.duckdns.org/webhook/post-intanizzudin';
   isLoading = false;
   message: { text: string; type: 'success' | 'error' } | null = null;
 
@@ -109,7 +109,7 @@ export class RSVPFirebaseService {
   async getRSVPs(): Promise<RSVPData[]> {
     try {
       console.log('Fetching RSVPs from API...');
-      const response = await fetch('https://normansamsudin.duckdns.org/webhook/get-muazzshafiqah');
+      const response = await fetch('https://normansamsudin.duckdns.org/webhook/get-intanizzudin');
       
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
